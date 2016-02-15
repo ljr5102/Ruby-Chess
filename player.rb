40cc0a2ac3218fae632262a6039355a649f40406
ljr5102 <ljr5102@gmail.com>
2016-02-15 13:32:59 -0500

    Add Display class

---
+++ player.rb
@@ -0,0 +1,21 @@
+# class Player
+#   attr_reader :board
+#   def initialize(name, board)
+#     @name = name
+#     @board = board
+#   end
+#
+#   def get_input
+#     puts "Input Start Position"
+#     enter_input
+#     puts "Input End Position"
+#     enter_input
+#
+#   end
+#
+#   def enter_input
+#     input = gets.chomp
+#
+#   end
+#
+# end
