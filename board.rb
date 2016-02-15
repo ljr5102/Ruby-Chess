15bd1e4b79cb4dfbb90bdd1ef4f2d0f256d25803
ljr5102 <ljr5102@gmail.com>
2016-02-15 13:32:19 -0500

    Add Board Class

---
+++ board.rb
@@ -0,0 +1,24 @@
+require "./exceptions"
+class Board
+  attr_accessor :grid
+  def initialize
+    @grid = Array.new(8) { Array.new(8) }
+
+  end
+
+  def [](pos)
+    @grid[pos[0]][pos[1]]
+  end
+
+  def []=(pos, value)
+    @grid[pos[0]][pos[1]] = value
+  end
+
+  def move(start_pos, end_pos)
+    piece = self[start_pos]
+    raise ChessGameError.new("No piece at #{start_pos}") if piece.nil?
+    #raise ChessGameError.new("Invalid move at end_pos") if can't move to end_pos
+    self[end_pos] = piece
+    self[start_pos] = nil
+  end
+end
